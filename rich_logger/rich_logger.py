dac88d6ac6b0916203cca804b8284df9359e560b
Elias Brohammer <37291741+EliasBrohammer@users.noreply.github.com>
2024-12-25 11:23:47 +0100

    Feat: logging with Rich works, but once defined it always prints to console

--- rich_logger/rich_logger.py
+++ rich_logger/rich_logger.py
@@ -1,11 +1,45 @@
-import rich
+import configparser
+import logging
+
+from rich.console import Console
+from rich.logging import RichHandler
 
 from rich_logger.defaults import DEFAULT_LOG_CONFIG
 
 
 class RichLogger:
-    def __init__(self):
-        pass
+    def __init__(
+        self,
+        logger_configuration_file: str = "",
+        logger_name: str = "rich_logger",
+        logging_to_console: bool = True,
+    ):
 
-    def info(self, message: str):
-        print(message)
+        user_configuration = {}
+        # Load the given configuration file
+        if len(logger_configuration_file) > 0:
+            parser = configparser.ConfigParser()
+            parser.read(logger_configuration_file)
+            user_configuration = {
+                key: parser.get("logging", key, fallback=None) for key in parser["logging"]
+            }
+
+        merged_configurations = {**DEFAULT_LOG_CONFIG, **user_configuration}
+
+        self.logger = logging.getLogger(logger_name)
+        self.formatter = logging.Formatter(merged_configurations["logger_format"])
+        self.logger.setLevel(merged_configurations["level"])
+
+        if logging_to_console:
+            self.console_handler = RichHandler(
+                console=Console(),
+                show_level=False,
+                show_time=False,
+                show_path=False,
+            )
+            self.console_handler.setFormatter(self.formatter)
+            self.logger.addHandler(self.console_handler)
+
+    def __getattr__(self, name):
+        """Delegate method calls to the logger instance."""
+        return getattr(self.logger, name)
