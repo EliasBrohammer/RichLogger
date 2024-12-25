8a3063ce56087cc8600a5a840d07240286b646fb
Elias Brohammer <37291741+EliasBrohammer@users.noreply.github.com>
2024-12-25 11:22:48 +0100

    Style: fixed style issue (in my point of view)

--- rich_logger/defaults.py
+++ rich_logger/defaults.py
@@ -2,7 +2,7 @@ import logging
 
 DEFAULT_LOG_CONFIG = {
     "level": logging.DEBUG,
-    "logger_format": "%(asctime)s - %(name)s - [%(levelname)s] - %(message)s",
+    "logger_format": "%(asctime)s - %(name)s - %(levelname)s - %(message)s",
     "date_format": "%Y-%m-%d %H:%M:%S",
     "max_bytes": 1000000,
     "backup_count": 5,
